687dd957626adcdde596344ecf697d8f48514650
Nalishaa <nalishashakya@gmail.com>
2025-09-18 18:28:56 +0545

    practice: string manipulation

--- Day 01/task_2.py
+++ Day 01/task_2.py
@@ -1,8 +1,8 @@
-# 1. Use \n to add another line of "Hello world".
+# Use \n to add another line of "Hello world".
 print("Hello world!\nHello world!\nHello world!\n")
 
 print("Hello"+"Angela")
-# 2. Add a space between the strings
+# Add a space between the strings
 print("Hello "+"Angela")
 print("Hello"+" Angela")
 print("Hello"+" "+"Angela")
