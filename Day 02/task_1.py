9d1ce52a0cc11f882533343cfdb768d597ad6e05
Nalishaa <nalishashakya@gmail.com>
2025-09-18 20:37:33 +0545

    practice: type checking and conversion

---
+++ Day 02/task_1.py
@@ -0,0 +1,11 @@
+# Type checking
+print(type("Hello"))
+print(type(123))
+print(type(12.32))
+print(type(True))
+
+# Type conversion
+# print("Number of letters in your name: " + len(input("Enter your name")))
+name=input("Enter your name")
+length=len(name)
+print("Number of letters in your name: " + str(length))
