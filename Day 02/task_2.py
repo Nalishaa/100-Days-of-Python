d6e3d52ee9903e90a2d6aca0e8ea909c6e6d522b
Nalishaa <nalishashakya@gmail.com>
2025-09-19 15:27:14 +0545

    practice: number manipulation

---
+++ Day 02/task_2.py
@@ -0,0 +1,28 @@
+bmi = 84 / 1.65 ** 2
+
+# Original Float with decimal places
+print(bmi)
+
+# Flooring the number by converting it into int
+print(int(bmi))
+
+# Rounding the number into a whole number
+print(round(bmi))
+
+# Rounding only to 2 decimal places
+print(round(bmi, 2))
+
+# Assignment Operators
+score = 0
+score += 1
+score -= 1
+score *= 2
+score /= 2
+
+# f-strings
+score = 0
+height = 1.8
+is_winning = True
+print(f"Your score is = {score}, your height is {height}. You are winning is {is_winning}")
+
+
