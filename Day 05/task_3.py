3dbed43e159cb28fcdfd2e3c9ff5466d251f425d
Nalishaa <nalishashakya@gmail.com>
2025-09-20 21:12:04 +0545

    practice: for loops with range

---
+++ Day 05/task_3.py
@@ -0,0 +1,10 @@
+for number in range(1,10):
+    print(number)
+
+for number in range(1,10,2):
+    print(number)
+
+sum_1_to_100=0
+for number in range(1,101):
+    sum_1_to_100+=number
+print (sum_1_to_100)
