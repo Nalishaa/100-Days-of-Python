81e2a69af054ccc884072bfb841a811c613af319
Nalishaa <nalishashakya@gmail.com>
2025-09-20 20:58:06 +0545

    practice: finding the highest score

---
+++ Day 05/task_2.py
@@ -0,0 +1,10 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+# Using built-in function
+print(max(student_scores))
+
+# Implementing the logic
+max_score=student_scores[0]
+for score in student_scores:
+    if score>max_score:
+        max_score=score
+print(max_score)
