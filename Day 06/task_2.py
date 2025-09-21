7cbef5a4114caba21c16b283980a4b8f991c8807
Nalishaa <nalishashakya@gmail.com>
2025-09-21 12:09:55 +0545

    practice: Reeborg Hurdle 1

---
+++ Day 06/task_2.py
@@ -0,0 +1,21 @@
+# Reeborg Hurdle 1 Solution
+# Note: Requires Reeborg's World environment to run
+# URL: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for i in range(6):
+    jump()
