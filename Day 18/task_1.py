01db1f90b12d65818eece21381630688cab86582
Nalishaa <nalishashakya@gmail.com>
2025-11-13 07:28:33 +0545

    practice: turtle challenge 1

---
+++ Day 18/task_1.py
@@ -0,0 +1,13 @@
+import turtle
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("DarkGreen")
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+screen = Screen()
+screen.exitonclick()
